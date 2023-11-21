60de45458e907d395c5eaf51956a08de1484f620
Thomas Melchers <thomas.melchers@gmail.com>
2023-11-21 09:48:19 +0100

    creation of base url and post setup

---
+++ client/src/api/axios.tsx
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const BASE_URL = "http://localhost:5000"
+
+export default axios.create({ baseURL: BASE_URL })
+
+// adding some axios interceptor which will work if the first response status is 403
+// will refresh the token if it has expired
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+})
