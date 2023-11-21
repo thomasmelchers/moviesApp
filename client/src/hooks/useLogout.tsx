9db598e3d15f6b4064d1bc732fae65698fcd6e5d
Thomas Melchers <thomas.melchers@gmail.com>
2023-11-21 09:56:00 +0100

    creation of useLogout

---
+++ client/src/hooks/useLogout.tsx
@@ -0,0 +1,23 @@
+import axios from "../api/axios"
+import useAuth from "./useAuth"
+import { initialAuthState } from "../context/AuthProvider"
+
+const useLogout = () => {
+  const { setAuth } = useAuth()
+
+  const logout = async () => {
+    setAuth(initialAuthState)
+
+    try {
+      const response = await axios("/logout", {
+        withCredentials: true,
+      })
+    } catch (err: any) {
+      console.error(err)
+    }
+  }
+
+  return logout
+}
+
+export default useLogout
