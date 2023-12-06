127bf3980da6547327ca90a0b9e2015747f8ad0f
Thomas Melchers <thomas.melchers@gmail.com>
2023-12-06 10:59:57 +0100

    UI: new userInterceptor hooks

---
+++ client/src/hooks/useInterceptorAxios.tsx
@@ -0,0 +1,52 @@
+import { axiosPrivate } from "../api/axios"
+import { useEffect } from "react"
+import useRefreshToken from "./useRefreshToken"
+import useAuth from "./useAuth"
+
+// This is to attach the interceptor
+
+const useInterceptorAxios = () => {
+  const refresh = useRefreshToken()
+  const { auth } = useAuth()
+
+  useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      (config) => {
+        // If the header authorization doesn't exist => it is not a retry
+        if (!config.headers["Authorization"]) {
+          // this is the actual accessToken
+          config.headers["Authorization"] = `Bearer ${auth?.accessToken}`
+        }
+        return config
+      },
+      (error) => Promise.reject(error)
+    )
+
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      // check if the response is OK
+      (response) => response,
+      // if the accessToken has expired => go to error
+      async (error) => {
+        const prevRequest = error?.config
+        // check if it has not been sent. Avoid infinite loop!
+        if (error?.response?.status === 403 && !prevRequest?.sent) {
+          prevRequest.sent = true
+          const newAccessToken = await refresh()
+          prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`
+          return axiosPrivate(prevRequest)
+        }
+        return Promise.reject(error)
+      }
+    )
+
+    // clean up function. Remove the interceptor
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept)
+      axiosPrivate.interceptors.response.eject(responseIntercept)
+    }
+  }, [auth, refresh])
+
+  return axiosPrivate
+}
+
+export default useInterceptorAxios
