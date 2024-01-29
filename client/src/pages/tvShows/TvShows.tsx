2b6547992f62b9e40b09b1d42c4c736bd766818f
Thomas Melchers <thomas.melchers@gmail.com>
2024-01-29 18:10:01 +0100

    update tvShows file name

--- client/src/pages/tvShows/TvShows.tsx
+++ client/src/pages/tvShows/TvShows.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { Grid } from '@mui/material'
+import useTMDBApiGenres from '../../hooks/useTMDBApiGenres'
+import ProductsTypeRow from '../../components/shared/productsTypeRow/ProductsTypeRow'
 import './tvShows.scss'
+import Spinner from '../../components/shared/spinner/Spinner'
 
-const series = () => {
-    return <div></div>
+const TvShows = () => {
+    const productType = 'tv'
+
+    const { loading, error, listOfSelectedGenres } = useTMDBApiGenres(
+        productType,
+        5,
+    )
+
+    const rowOfSeries = listOfSelectedGenres?.map((genre) => (
+        <ProductsTypeRow
+            key={genre.name}
+            productsTypeName={genre.name}
+            productsTypeApiUrl={genre.url}
+            productsTypeGenreId={genre.id}
+            productType={productType}
+        />
+    ))
+
+    return (
+        <Grid
+            container
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+        >
+            {error ? <p>{error}</p> : null}
+            {loading && !error ? <Spinner /> : rowOfSeries}
+        </Grid>
+    )
 }
 
-export default series
+export default TvShows
