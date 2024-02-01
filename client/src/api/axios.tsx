e64d11457694d92db7901803bbaeb1dd3669ac4b
Thomas Melchers <thomas.melchers@gmail.com>
2024-02-01 22:13:55 +0100

    make ready for deployement

--- client/src/api/axios.tsx
+++ client/src/api/axios.tsx
@@ -1,13 +1,16 @@
-import axios from "axios"
+import axios from 'axios'
 
-const BASE_URL = "http://localhost:5000/api/v1"
+const BASE_URL =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:5000/api/v1'
+        : 'https://movies-app-server.vercel.app'
 
 export default axios.create({ baseURL: BASE_URL })
 
 // adding some axios interceptor which will work if the first response status is 403
 // will refresh the token if it has expired
 export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
-  headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
 })
