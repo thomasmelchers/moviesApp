81eee4d93dc5153921d78c8e4c99a9ca6656ef58
Thomas Melchers <thomas.melchers@gmail.com>
2024-02-01 22:44:35 +0100

    second test

--- api/src/routes/testRoutes.ts
+++ api/src/routes/testRoutes.ts
@@ -4,6 +4,7 @@ const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
     console.log('test')
+    console.log('client:', process.env.CLIENT_URL, 'port:', process.env.PORT)
     res.status(201).json({ testRoute: 'test' })
 })
 
