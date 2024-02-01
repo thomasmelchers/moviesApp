24a0152d0f6d69fda5d7c37f52ddf3a61353b0c3
Thomas Melchers <thomas.melchers@gmail.com>
2024-02-01 23:40:43 +0100

    vercel solution

--- api/src/server.ts
+++ api/src/server.ts
@@ -19,6 +19,29 @@ const app = express()
 const PORT = process.env.PORT || 5000
 console.log('client:', process.env.CLIENT_URL, 'port:', process.env.PORT)
 
+app.use(
+    (fn: (req: Request, res: Response) => Promise<void>) =>
+        async (req: Request, res: Response) => {
+            res.setHeader('Access-Control-Allow-Credentials', 'true')
+            res.setHeader('Access-Control-Allow-Origin', '*')
+            // another common pattern
+            // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+            res.setHeader(
+                'Access-Control-Allow-Methods',
+                'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            )
+            res.setHeader(
+                'Access-Control-Allow-Headers',
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            )
+            if (req.method === 'OPTIONS') {
+                res.status(200).end()
+                return
+            }
+            return await fn(req, res)
+        },
+)
+
 // app.use(
 //     cors({
 //         origin:
