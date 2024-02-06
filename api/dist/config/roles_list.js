9d22d972d2c3957685b24bac54d9a73dfbf597da
Thomas Melchers <thomas.melchers@gmail.com>
2024-02-06 11:07:59 +0100

    removing dist folder

--- api/dist/config/roles_list.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ROLES_LIST;
-(function (ROLES_LIST) {
-    ROLES_LIST[ROLES_LIST["Admin"] = 3000] = "Admin";
-    ROLES_LIST[ROLES_LIST["Editor"] = 2000] = "Editor";
-    ROLES_LIST[ROLES_LIST["User"] = 1000] = "User";
-})(ROLES_LIST || (ROLES_LIST = {}));
-exports.default = ROLES_LIST;
