978e47f77ba7138ab29227e19cdd5f07e63bdbe9
Thomas Melchers <thomas.melchers@gmail.com>
2024-02-08 09:23:39 +0100

    navbar for small device

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -3,6 +3,11 @@ import { useNavigate, Link } from 'react-router-dom'
 import useLogout from '../../hooks/useLogout'
 import useAuth from '../../hooks/useAuth'
 import { Grid } from '@mui/material'
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded'
+import { VideocamRounded } from '@mui/icons-material'
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded'
+import LiveTvRoundedIcon from '@mui/icons-material/LiveTvRounded'
+import ExitToAppRoundedIcon from '@mui/icons-material/ExitToAppRounded'
 import './navBar.scss'
 
 const NavBar = () => {
@@ -18,10 +23,12 @@ const NavBar = () => {
 
     return (
         <Grid container className="container">
-            <Grid item>
-                <div className="title">
-                    <h1>TomFlix</h1>
-                </div>
+            <Grid item xs={12} md={2}>
+                <Link to="/home" className="title-link">
+                    <div className="title">
+                        <h1>TomFlix</h1>
+                    </div>
+                </Link>
             </Grid>
             <Grid
                 item
@@ -30,19 +37,48 @@ const NavBar = () => {
             >
                 {auth?.username ? (
                     <div className="menu__container">
-                        <Link to="/home" className="menu__link">
-                            Home
-                        </Link>
-                        <Link to="/movies" className="menu__link">
-                            Movies
-                        </Link>
-                        <Link to="/tv-shows" className="menu__link">
-                            Series
-                        </Link>
-                        <Link to={`/profile/${auth.id}`} className="menu__link">
-                            Profile
-                        </Link>
+                        <Link to="/home">Home</Link>
+                        <Link to="/movies">Movies</Link>
+                        <Link to="/tv-shows">Series</Link>
+                        <Link to={`/profile/${auth.id}`}>Profile</Link>
                         <button onClick={signOut}>Logout</button>
+                    </div>
+                ) : (
+                    <div className="menu__container__not-login">
+                        <Link to="/login">
+                            <button>Login</button>
+                        </Link>
+                    </div>
+                )}
+            </Grid>
+
+            {/* SMALL */}
+            <Grid
+                item
+                sx={{
+                    display: { xs: 'flex', md: 'none' },
+                }}
+                className="container-xs"
+            >
+                {auth?.username ? (
+                    <div className="menu__container link__xs">
+                        <Link to="/home">
+                            <HomeRoundedIcon />
+                        </Link>
+                        <Link to="/movies">
+                            <VideocamRounded />
+                        </Link>
+                        <Link to="/tv-shows">
+                            <LiveTvRoundedIcon />
+                        </Link>
+                        <Link to={`/profile/${auth.id}`}>
+                            <PersonRoundedIcon />
+                        </Link>
+
+                        <ExitToAppRoundedIcon
+                            onClick={signOut}
+                            className="logout"
+                        />
                     </div>
                 ) : (
                     <div className="menu__container__not-login">
