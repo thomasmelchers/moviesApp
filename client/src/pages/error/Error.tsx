73fd4ff8a5dc085ed4d30b79e75e3df12c38adb4
Thomas Melchers <thomas.melchers@gmail.com>
2024-03-12 12:18:32 +0100

    update error page layout

--- client/src/pages/error/Error.tsx
+++ client/src/pages/error/Error.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './notFound.scss'
+import useAuth from '../../hooks/useAuth'
 
-const NotFound = () => {
+const Error = () => {
     return (
         <div className="not-found__container">
             <h2>This page is not found</h2>
@@ -13,4 +14,4 @@ const NotFound = () => {
     )
 }
 
-export default NotFound
+export default Error
