892a2fb7e6dc88171ca11ac077bcb946550c0d3b
Thomas Melchers <thomas.melchers@gmail.com>
2024-06-04 22:20:20 +0200

    changing api url

--- client/src/api/axios.tsx
+++ client/src/api/axios.tsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const BASE_URL =
     process.env.NODE_ENV === 'development'
         ? 'http://localhost:5000/api/v1'
-        : 'https://movies-app-api-4gj0.onrender.com/api/v1'
+        : 'https://movies-app-api-o8ru.onrender.com/api/v1'
 // : 'https://tomflix-api.cyclic.app/api/v1'
 
 export default axios.create({ baseURL: BASE_URL })
